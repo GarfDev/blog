ecdd3c5306e320aea2110ce417ff5850b7094435
GarfDev <garfdev.13@gmail.com>
2021-06-12 21:21:49 +0700

    feat: Add OG for Application

--- application/index.tsx
+++ application/index.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Base } from "./@global/styles";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import { Helmet } from "react-helmet";
+// CSS
+import "bootstrap/dist/css/bootstrap.min.css";
 // Import Internal Features
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Homepage from "./homepage";
 
 const Application = () => {
   return (
     <>
+      <Helmet>
+        <meta property="og:url" content="https://garfdev.netlify.app/" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="Nguyen Gia Tuan Personal Space" />
+      </Helmet>
       <Router>
         <Switch>
           <Route path="/" component={Homepage} />
